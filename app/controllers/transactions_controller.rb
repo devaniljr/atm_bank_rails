c887a057b789beaf36f0f6fdbd9d69bb8756e2c1
devaniljr <contatodevanil@gmail.com>
2022-07-20 15:11:29 -0400

    Fix Email Not found on Transference

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -56,22 +56,24 @@ class TransactionsController < ApplicationController
     @transaction.type_of_transaction = "Transferência"
     @transaction.account = Account.find(session[:user_id])
 
-    p @transaction.save
-
     if @transaction.save
-      destination = Account.find_by!(email: @transaction.destination)
+      destination = Account.find_by(email: @transaction.destination)
       sender = @transaction.account
       @transaction.transference_tax = Transaction.transference_tax(@transaction.amount.to_f, Time.now)
 
-      if destination && sender && destination != sender && @transaction.amount.to_f + @transaction.transference_tax <= @transaction.account.balance
-        destination.balance += @transaction.amount.to_f
-        sender.balance -= @transaction.amount.to_f + Transaction.transference_tax(@transaction.amount.to_f, Time.now)
-        destination.save
-        sender.save
-        @transaction.save
-        redirect_to transaction_receipt_path(@transaction), notice: "Depósito realizado com sucesso"
+      if destination
+        if sender && destination != sender && @transaction.amount.to_f + @transaction.transference_tax <= @transaction.account.balance
+          destination.balance += @transaction.amount.to_f
+          sender.balance -= @transaction.amount.to_f + Transaction.transference_tax(@transaction.amount.to_f, Time.now)
+          destination.save
+          sender.save
+          @transaction.save
+          redirect_to transaction_receipt_path(@transaction), notice: "Depósito realizado com sucesso"
+        else
+          redirect_to transference_path, status: :unprocessable_entity, alert: "Saldo insuficiente para este saque!"
+        end
       else
-        redirect_to transference_path, status: :unprocessable_entity, alert: "Saldo insuficiente para este saque!"
+        redirect_to transference_path, status: :unprocessable_entity, alert: "E-mail do destinatário não encontrado!"
       end
     else
       redirect_to transference_path, status: :unprocessable_entity, alert: "Não foi possível realizar sua transação!"
